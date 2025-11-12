fcd86ff0b62057a9a43d481d6f092ee2b77f041a
Timothy Angus <timangus321@gmail.com>
2025-11-12 11:47:02 +0000

    add first icon__button to sidebar and create basic layout for desktop style

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,10 +1,10 @@
 import ButtonIconProps from "./IconButton.types";
+import styles from "./iconButton.module.scss";
 
 export default function IconButton({ icon: Icon }: ButtonIconProps) {
   return (
-      <button
-      >
-        <Icon />
-      </button>
+    <button className={styles.icon__button}>
+      <Icon />
+    </button>
   );
 }
